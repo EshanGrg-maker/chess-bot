f824534a111198b1f482a1dc0e5864a2e3d6955a
EshanGrg-maker <eshangurug@gmail.com>
2026-09-09 15:34:06 +0100

    pawn movement complete (no promotion yet)

--- Stuff/Engine/ChessEngine.cpp
+++ Stuff/Engine/ChessEngine.cpp
@@ -120,7 +120,19 @@ void Piece::calculate_legal_moves()
             int delta_y = -1;
             int limit = delta_y;
             if (last_square.x == -1) {limit *= 2;}
-            for (int y = delta_y; y >= limit; y += delta_y) {legal_moves.push_back(square(current_square.x,current_square.y + y));}
+            for (int y = delta_y; y >= limit; y += delta_y) 
+            {
+                square sqr = square(current_square.x,current_square.y + y);
+                if (board[(sqr.y*8)+sqr.x] == nullptr) {legal_moves.push_back(sqr);} 
+            }
+            for (int x = -1; x <= 1; x++)
+            {\
+                if (x==0) {continue;}
+                square sqr = square(current_square.x + x,current_square.y + delta_y);
+                int index = (sqr.y*8)+sqr.x;
+                if (board[index] != nullptr && board[index]->piece_index != piece_index) {legal_moves.push_back(sqr);} 
+            }
+
             break;
     }
 };
